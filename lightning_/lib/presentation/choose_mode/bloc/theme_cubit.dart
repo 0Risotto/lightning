b095e2e5bd6b4bd1b9ab5dbe5bd18d5f90c02230
0Risotto <anasjallad19@gmail.com>
2025-05-02 11:20:08 +0300

    second

--- lightning_/lib/presentation/choose_mode/bloc/theme_cubit.dart
+++ lightning_/lib/presentation/choose_mode/bloc/theme_cubit.dart
@@ -1,23 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 
-class ThemeCubit extends HydratedCubit <ThemeMode>{
-  
-  ThemeCubit() : super (ThemeMode.system);
+class ThemeCubit extends HydratedCubit<ThemeMode> {
+  ThemeCubit() : super(ThemeMode.light);
 
+  /// Updates the current theme mode
   void updateTheme(ThemeMode themeMode) => emit(themeMode);
-  
 
+  /// Deserialize the theme mode from stored JSON
   @override
   ThemeMode? fromJson(Map<String, dynamic> json) {
-    // TODO: implement fromJson
-    throw UnimplementedError();
+    final theme = json['theme'] as String?;
+    switch (theme) {
+      case 'light':
+        return ThemeMode.light;
+      case 'dark':
+        return ThemeMode.dark;
+      case 'system':
+      default:
+        return ThemeMode.system;
+    }
   }
 
+  /// Serialize the theme mode into JSON
   @override
   Map<String, dynamic>? toJson(ThemeMode state) {
-    // TODO: implement toJson
-    throw UnimplementedError();
+    late final String theme;
+    switch (state) {
+      case ThemeMode.light:
+        theme = 'light';
+        break;
+      case ThemeMode.dark:
+        theme = 'dark';
+        break;
+      case ThemeMode.system:
+        theme = 'system';
+        break;
+    }
+    return {'theme': theme};
   }
-
 }
